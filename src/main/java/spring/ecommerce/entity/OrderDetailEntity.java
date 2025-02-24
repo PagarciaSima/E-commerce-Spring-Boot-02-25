01a3397557df61c5263d5679a61d5d88a33c088a
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-02-24 13:38:05 +0100

    OrderDetail entity

--- src/main/java/spring/ecommerce/entity/OrderDetailEntity.java
+++ src/main/java/spring/ecommerce/entity/OrderDetailEntity.java
@@ -1,5 +1,37 @@
 package spring.ecommerce.entity;
 
-public class OrderDetailEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "order-detail")
+@Data
+@NoArgsConstructor
+public class OrderDetailEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer orderId;
+	private String orderFullName;
+	private String orderFullOrder;
+	private String orderContactNumber;
+	private String orderAlternateContactNumber;
+	private String orderStatus;
+	private Double orderAmount;
+	
+	public OrderDetailEntity(String orderFullName, String orderFullOrder, String orderContactNumber,
+			String orderAlternateContactNumber, String orderStatus, Double orderAmount) {
+		super();
+		this.orderFullName = orderFullName;
+		this.orderFullOrder = orderFullOrder;
+		this.orderContactNumber = orderContactNumber;
+		this.orderAlternateContactNumber = orderAlternateContactNumber;
+		this.orderStatus = orderStatus;
+		this.orderAmount = orderAmount;
+	}
 }
