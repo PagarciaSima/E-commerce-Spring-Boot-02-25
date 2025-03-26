9ab648b53764b70c90e185d634fd6f33d0817ce8
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-03-26 13:06:27 +0100

    Adding new prod env to open api doc

--- src/main/java/spring/ecommerce/configuration/OpenApiConfiguration.java
+++ src/main/java/spring/ecommerce/configuration/OpenApiConfiguration.java
@@ -23,7 +23,11 @@ import io.swagger.v3.oas.annotations.servers.Server;
 		@Server(
 			description = "Local ENV",
 			url = "http://localhost:8081"
-		)
+		),
+		@Server(
+            description = "Production ENV",
+            url = "https://pgsecommerce02-25-production.up.railway.app"
+        )
 	}
 )
 
